a24e3d584a860ce88147558a896a4e1cbbc43579
Ryan Campbell <ryan.campbell@dashhudson.com>
2020-02-04 14:14:39 -0400

    Attempting to lower verbosity
    We dont need the build logs spammed with XMLs

--- veracode_submit_scan.sh
+++ veracode_submit_scan.sh
@@ -7,11 +7,9 @@
 #       is downloaded if the python version is not sufficient. 
 sudo snap install xmlstarlet
 if [ "$TRAVIS_PYTHON_VERSION" >= "3.0" ]; then
-    echo "Triggering python build"
     pip install httpie
     pip install veracode-api-signing
 else 
-    echo "Triggering node build"
     sudo pip install httpie==0.9.9
     sudo pip install veracode-api-signing
 fi
@@ -23,19 +21,17 @@ cat applist.xml
 
 # Parsing the XML for the `app_id` where `app_name == $STACK_NAME`
 export APP_ID=$( xmlstarlet sel -N oe="http&#x3a;&#x2f;&#x2f;www.w3.org&#x2f;2001&#x2f;XMLSchema-instance" -N ve="https&#x3a;&#x2f;&#x2f;analysiscenter.veracode.com&#x2f;schema&#x2f;2.0&#x2f;applist" xsi:schemaLocation="https&#x3a;&#x2f;&#x2f;analysiscenter.veracode.com&#x2f;schema&#x2f;2.0&#x2f;applist https&#x3a;&#x2f;&#x2f;analysiscenter.veracode.com&#x2f;resource&#x2f;2.0&#x2f;applist.xsd" --net -t -v '//*[@app_name="'$STACK_NAME'"]/@app_id' -n applist.xml )
-echo "This app's veradoce ID: "$APP_ID
 
 # zip all .py .js .vue files except those in /tests/ or /__tests__/
 # TODO: May be useful to implement a `.veracodeignore` or something similar to allow
 #       developers to specify files that shouldn't be scanned.
+echo "Collecting files for Veracode submission"
 zip -R veracode_submission.zip \
                 '*.py' '*.js' '*.vue' 'requirements.txt' 'package-lock.json' 'Pipfile.lock' 'Pipfile' \
                 -x /**\*tests/**\* /**\*__tests__/**\* /**\*node_modules/**\* /**\*cypress/**\*
 
-echo "Uploading file"
-http --ignore-stdin --auth-type=veracode_hmac -f "https://analysiscenter.veracode.com/api/5.0/uploadfile.do" "app_id==$APP_ID" "file@veracode_submission.zip"
+http --ignore-stdin --auth-type=veracode_hmac -f "https://analysiscenter.veracode.com/api/5.0/uploadfile.do" "app_id==$APP_ID" "file@veracode_submission.zip" > /dev/null 2>&1
 
 # Begin pre-scan
 # With auto_scan=true the full scan will be triggered after the pre-scan using the veracode default modules
-echo "Triggering scan"
-http --auth-type=veracode_hmac "https://analysiscenter.veracode.com/api/5.0/beginprescan.do" "app_id==$APP_ID" "auto_scan==true"
+http --auth-type=veracode_hmac "https://analysiscenter.veracode.com/api/5.0/beginprescan.do" "app_id==$APP_ID" "auto_scan==true" > /dev/null 2>&1
