1f7d4653d4bd20d7758a2ab2ac2066ba3115d3b2
Ryan Campbell <ryan.campbell@dashhudson.com>
2020-02-10 12:15:01 -0400

    Converting from applications to sandboxes

--- veracode_submit_scan.sh
+++ veracode_submit_scan.sh
@@ -14,23 +14,32 @@ else
     sudo pip install veracode-api-signing
 fi
 
+APP_ID=696274
 
-# Getting a list of all our apps present in veracode and saving it as an XML
-##http --auth-type=veracode_hmac "https://analysiscenter.veracode.com/api/5.0/getapplist.do" > applist.xml
+# Creating the sandbox that the files will be uploaded to. 
+#
+http --auth-type=veracode_hmac "https://analysiscenter.veracode.com/api/5.0/createsandbox.do" "app_id==$APP_ID" "sandbox_name==$STACK_NAME"
+
+# Getting a list of all our sandboxes present in veracode and saving it as an XML
+http --auth-type=veracode_hmac "https://analysiscenter.veracode.com/api/5.0/getsandboxlist.do" "app_id==$APP_ID" > applist.xml
 
 # Parsing the XML for the `app_id` where `app_name == $STACK_NAME`
-##export APP_ID=$( xmlstarlet sel -N oe="http&#x3a;&#x2f;&#x2f;www.w3.org&#x2f;2001&#x2f;XMLSchema-instance" -N ve="https&#x3a;&#x2f;&#x2f;analysiscenter.veracode.com&#x2f;schema&#x2f;2.0&#x2f;applist" xsi:schemaLocation="https&#x3a;&#x2f;&#x2f;analysiscenter.veracode.com&#x2f;schema&#x2f;2.0&#x2f;applist https&#x3a;&#x2f;&#x2f;analysiscenter.veracode.com&#x2f;resource&#x2f;2.0&#x2f;applist.xsd" --net -t -v '//*[@app_name="'$STACK_NAME'"]/@app_id' -n applist.xml )
+export SANDBOX_ID=$( xmlstarlet sel -N 
+    oe="http&#x3a;&#x2f;&#x2f;www.w3.org&#x2f;2001&#x2f;XMLSchema-instance" -N 
+    ve="https&#x3a;&#x2f;&#x2f;analysiscenter.veracode.com&#x2f;schema&#x2f;4.0&#x2f;sandboxlist" 
+    xsi:schemaLocation="https&#x3a;&#x2f;&#x2f;analysiscenter.veracode.com&#x2f;schema&#x2f;4.0&#x2f;sandboxlist https&#x3a;&#x2f;&#x2f;analysiscenter.veracode.com&#x2f;resource&#x2f;4.0&#x2f;sandboxlist.xsd" 
+    --net -t -v '//*[@sandbox_name="'$STACK_NAME'"]/@sandbox_id' -n applist.xml )
 
 # zip all .py .js .vue files except those in /tests/ or /__tests__/
 # TODO: May be useful to implement a `.veracodeignore` or something similar to allow
 #       developers to specify files that shouldn't be scanned.
 echo "Collecting files for Veracode submission"
-##zip -R veracode_submission.zip \
-##                '*.py' '*.js' '*.vue' 'requirements.txt' 'package-lock.json' 'Pipfile.lock' 'Pipfile' \
-##                -x /**\*tests/**\* /**\*__tests__/**\* /**\*node_modules/**\* /**\*cypress/**\*
+zip -R veracode_submission.zip \
+                '*.py' '*.js' '*.vue' 'requirements.txt' 'package-lock.json' 'Pipfile.lock' 'Pipfile' \
+                -x /**\*tests/**\* /**\*__tests__/**\* /**\*node_modules/**\* /**\*cypress/**\*
 
-##http --ignore-stdin --auth-type=veracode_hmac -f "https://analysiscenter.veracode.com/api/5.0/uploadfile.do" "app_id==$APP_ID" "file@veracode_submission.zip" > /dev/null 2>&1
+http --ignore-stdin --auth-type=veracode_hmac -f "https://analysiscenter.veracode.com/api/5.0/uploadfile.do" "app_id==$APP_ID" "sandbox_id==$SANDBOX_ID" "file@veracode_submission.zip" > /dev/null 2>&1
 
 # Begin pre-scan
 # With auto_scan=true the full scan will be triggered after the pre-scan using the veracode default modules
-##http --auth-type=veracode_hmac "https://analysiscenter.veracode.com/api/5.0/beginprescan.do" "app_id==$APP_ID" "auto_scan==true" > /dev/null 2>&1
+http --auth-type=veracode_hmac "https://analysiscenter.veracode.com/api/5.0/beginprescan.do" "app_id==$APP_ID" "sandbox_id==$SANDBOX_ID" "auto_scan==true" > /dev/null 2>&1
